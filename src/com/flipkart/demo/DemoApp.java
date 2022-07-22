802030ef988359960c50bf6e8941084e08ab2c4a
Harshit Kumar Mittal <harshitkumar.m@BLRETV-C9WP92Y0.local>
2022-07-22 10:11:39 +0530

    Demo-Java-Project

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author harshitkumar.m
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Demo Project");
+
+	}
+
+}
